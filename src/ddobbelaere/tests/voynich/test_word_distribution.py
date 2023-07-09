61e92bf8c9eff310434af5b4917ddbb0c1f2712e
Dieter Dobbelaere <dieter.dobbelaere@gmail.com>
2023-07-09 12:27:44 +0200

    Extend word distribution test.

--- src/ddobbelaere/tests/voynich/test_word_distribution.py
+++ src/ddobbelaere/tests/voynich/test_word_distribution.py
@@ -3,6 +3,7 @@ Word distribution tests.
 """
 
 from collections import defaultdict
+from typing import Final
 
 from ddobbelaere.voynich import Manuscript
 
@@ -23,4 +24,32 @@ def test_word_distribution(manuscript: Manuscript):
 
     sorted_count = sorted(count.items(), key=lambda x: x[1], reverse=True)
 
-    print(sorted_count)
+    print(f"Found {len(set(words))} unique words out of {len(words)} total words")
+    print()
+
+    column_width: Final = 10
+    max_num_examples: Final = 20
+    print(
+        f"{'#times':<{column_width}}{'#words':<{column_width}}{'words':<{column_width}}"
+    )
+    print("-" * 3 * column_width)
+    for num_times in range(sorted_count[0][1], 0, -1):
+        # pylint:disable=cell-var-from-loop
+        filtered_words = list(filter(lambda x: x[1] == num_times, sorted_count))
+        num_words = len(filtered_words)
+
+        if num_words > 0:
+            example_words = ", ".join(
+                list(
+                    map(
+                        lambda x: x[0],
+                        filtered_words[: min(max_num_examples, num_words)],
+                    )
+                )
+            )
+            if num_words > max_num_examples:
+                example_words += "..."
+
+            print(
+                f"{num_times:<{column_width}}{num_words:<{column_width}}{example_words:<{column_width}}"
+            )
